7750ed24ef3311e2a2c931098922ce6fa75f737b
AjileVenkat <86122369+AjileVenkat@users.noreply.github.com>
2021-06-19 21:07:26 -0500

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,4 +1,36 @@
 
-public class CheckingAccount {
-
+public class CheckingAccount 
+{
+	private double amount;
+	
+	public CheckingAccount()
+	{
+		amount = 0;
+	}
+	
+	public CheckingAccount(double amt)
+	{
+		amount = amt;
+	}
+	
+	public void deposit(double amt)
+	{
+		amount += amt;
+	}
+	
+	public void transfer(double amt, CheckingAccount other)
+	{
+		this.amount -= amt;
+		other.amount += amt;
+	}
+	
+	public void withdraw(double amt)
+	{
+		amount -= amt;
+	}
+	
+	public double getBalance()
+	{
+		return amount;
+	}
 }
