ede69bca199e5cfebe135849fcca9c2417939daf
AjileVenkat <86122369+AjileVenkat@users.noreply.github.com>
2021-06-19 21:05:51 -0500

    Update CarLoan.java

--- CarLoan.java
+++ CarLoan.java
@@ -1,4 +1,19 @@
-
-public class CarLoan {
-
+public class CarLoan extends Loan
+{
+private static final double INTEREST_RATE = 0.04;
+	
+	public CarLoan(double amt)
+	{
+		super(amt, 5);
+	}
+	
+	public CarLoan(double amt, double years)
+	{
+		super(amt,years);
+	}
+	
+	public void calculate()
+	{
+		setAmount(getAmount() * Math.pow(1+INTEREST_RATE, getYears()));
+	}
 }
